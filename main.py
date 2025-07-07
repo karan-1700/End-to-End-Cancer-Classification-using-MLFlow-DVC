ac683b7f828dc3006abee014010ee55ef7c1daca
karan-1700 <padhiar.karan@gmail.com>
2025-07-07 16:22:13 -0600

    utils, exception and logger added

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.CNN_Classifier import logger # can also be imported as: `from CNN_Classifier import logger`
+
+logger.info("Welcome to the CNN_Classifier module!")
---
+++ src/CNN_Classifier/__init__.py
@@ -0,0 +1,23 @@
+# import necessary packages
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s] [%(levelname)s] [%(module)s] : %(message)s"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, "running_logs.log") # save logs to a file
+# create log directory if it doesn't exist
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = logging_str,
+
+    handlers = [
+        logging.FileHandler(log_filepath), # save logs to a file
+        logging.StreamHandler(sys.stdout) # also print logs to console/terminal
+    ]
+)
+
+logger = logging.getLogger("CNN_Classifier_Logger")
